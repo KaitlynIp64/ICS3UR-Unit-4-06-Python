33e551389f1930593cf7196abb18106805ce261b
Kaitlyn Ip <kaitlyn.ip@mths.ca>
2022-11-21 16:15:07 +0000

    added and fixed code

--- colour_loops.py
+++ colour_loops.py
@@ -4,18 +4,20 @@
 # Created on: Nov 2022
 # This program prints all possible RGB combination
 
+
 def main():
     # this function uses a nested loop
 
-    counter1 = 0
-    counter2 = 0
-    counter3 = 0
-
-    # process & output
-    for counter1 in range(255):
-        for counter2 in range(255):
-            for counter3 in range(255):
-                print("RGB(" {0} "," {1} "," {2}")".format(counter1, counter2, counter3))
+    red = 0
+    green = 0
+    blue = 0
+    for red in range(0, 255):
+        for green in range(0, 255):
+            for blue in range(0, 255):
+                print("RGB({0},{1},{2})".format(red, green, blue))
+                blue += 1
+            green += 1
+        red += 1
 
 
 if __name__ == "__main__":
